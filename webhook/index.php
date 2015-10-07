3d4b9c8547031836b3ab7317c8df83d8c866e1a6
zhy1stgg <823402333@qq.com>
2015-10-07 16:46:07 +0800

    update

--- webhook/index.php
+++ webhook/index.php
@@ -1,4 +1,20 @@
 <?php 
-	$valid_token = 'd49dfa7622681425fbcbdd687eb2ca59498ce852';
+	$valid_token = 'highcharts.com.cn';
+	$client_token = $_GET['token'];
+	$client_ip = $_SERVER['REMOTE_ADDR'];
+
+	$fs = fopen('./hook.log', 'a');
+	fwrite($fs, 'Request on ['.date("Y-m-d H:i:s").'] from ['.$client_ip.']'.PHP_EOL);
+	if ($client_token !== $valid_token)
+	{
+	    echo "error 10001";
+	    fwrite($fs, "Invalid token [{$client_token}]".PHP_EOL);
+	    exit(0);
+	}
+	$json = file_get_contents('php://input');
+	$data = json_decode($json, true);
+	fwrite($fs, 'Data: '.print_r($data, true).PHP_EOL);
+	fwrite($fs, '======================================================================='.PHP_EOL);
 	exec('sh hook.sh');
+	$fs and fclose($fs);
 ?>
