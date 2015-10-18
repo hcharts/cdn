fc0d5375ac3c644eb588fc8f60b08e9c0609dc10
zhy1stgg <823402333@qq.com>
2015-10-18 10:20:18 +0800

    update webhook

--- webhook/hook.sh
+++ webhook/hook.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-times=`date +'%Y-%m-%d %H:%M:%S'`
-echo  $times'   starting....' >> hook.log
+#/bin/bash
+#times=`date +'%Y-%m-%d %H:%M:%S'`
+#echo  $times'   starting....' >> hook.log
 cd ..
 sudo git pull origin master
-endTime=`date +'%Y-%m-%d %H:%M:%S'`
-echo $endTime'   done' >> webhook/hook.log
+#endTime=`date +'%Y-%m-%d %H:%M:%S'`
+#echo $endTime'   done' >> webhook/hook.log
 /www/hcharts.cn/qiniu/qrsync /www/hcharts.cn/qiniu/site/cdn.json
--- webhook/index.php
+++ webhook/index.php
@@ -2,20 +2,21 @@
 	$valid_token = 'highcharts.com.cn';
 	$client_token = $_GET['token'];
 	$client_ip = $_SERVER['REMOTE_ADDR'];
-/*
+
 	$fs = fopen('./hook.log', 'a');
 	fwrite($fs, 'Request on ['.date("Y-m-d H:i:s").'] from ['.$client_ip.']'.PHP_EOL);
 	fwrite($fs, print_r($_POST, true).PHP_EOL);
-	if ($client_token !== $valid_token)
+/*	if ($client_token !== $valid_token)
 	{
 	    echo "error 10001";
 	    fwrite($fs, "Invalid token [{$client_token}]".PHP_EOL);
 	    exit(0);
 	}
+*/
 	$json = file_get_contents('php://input');
 	$data = json_decode($json, true);
 	fwrite($fs, 'Data: '.print_r($data, true).PHP_EOL);
 	fwrite($fs, '======================================================================='.PHP_EOL);
-*/	exec('sh hook.sh');
+	exec('sh hook.sh');
 	$fs and fclose($fs);
 ?>
