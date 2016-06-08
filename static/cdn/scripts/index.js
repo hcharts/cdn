309b5745fa489b5b33d868e1fc500d18186eff8a
zhy1stgg <823402333@qq.com>
2016-06-08 23:25:08 +0800

    update

--- static/cdn/scripts/index.js
+++ static/cdn/scripts/index.js
@@ -1,1 +1,1 @@
-var hash=window.location.hash;(null!==hash||""!==hash)&&$('#hs-tab a[href="'+hash+'"]').tab("show"),jQuery('input[name="cdn"]').click(function(){jQuery(this).select()});
+var hash=window.location.hash;null===hash&&""===hash||$('#hs-tab a[href="'+hash+'"]').tab("show"),jQuery('input[name="cdn"]').click(function(){jQuery(this).select()});
