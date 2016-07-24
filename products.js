509ade3c444d4a18f3ef099e09aaba81410cc01f
root <root@localhost>
2016-07-24 23:45:09 +0800

    update products.js

---
+++ products.js
@@ -0,0 +1,16 @@
+
+            var products = {
+                "Highcharts": {
+                    "date": "2016-05-06", 
+                    "nr": "4.2.5"
+                },
+                "Highstock": {
+                    "date": "2016-05-06",
+                    "nr": "4.2.5"
+                },
+                "Highmaps": {
+                    "date": "2016-05-06",
+                    "nr": "4.2.5"
+                }
+            }
+        
